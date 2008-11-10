1bde51943231399cb3ae077c1362c8e8aaf9970d
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-11-10 13:28:01 +0000

    Localisation updates for extensions messages from Betawiki (2008-11-10 14:18 CET)

--- InputBox.i18n.php
+++ InputBox.i18n.php
@@ -183,8 +183,10 @@ $messages['bi'] = array(
 
 /** Bengali (বাংলা)
  * @author Bellayet
+ * @author Zaheen
  */
 $messages['bn'] = array(
+	'inputbox-desc' => 'পূর্বসংজ্ঞায়িত এইচটিএমএল ফর্মের অন্তর্ভুক্তি অনুমোদন করুন',
 	'inputbox-error-no-type' => 'আপনি ইনপুট বক্স তৈরির জন্য ইনপুট বক্সের ধরণ নির্ধারণ করেননি।',
 	'inputbox-error-bad-type' => '"$1" ধরণের ইনপুট বক্স নেই। দয়া করে "create", "comment", "search" অথবা "search2" নির্ধারণ করুন।',
 	'tryexact' => 'ঠিক এই নামের নিবন্ধে যান',
