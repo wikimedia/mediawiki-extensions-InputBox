f1e523997d58ac93e3b0d5ace347dd8592638be0
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-08-21 23:20:48 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-08-21 22:44 UTC)

--- InputBox.i18n.php
+++ InputBox.i18n.php
@@ -28,9 +28,10 @@ Please specify "create", "comment", "search", "search2" or "fulltext".',
  * @author Meno25
  * @author SPQRobin
  * @author Siebrand
+ * @author The Evil IP address
  */
 $messages['qqq'] = array(
-	'inputbox-desc' => 'Short description of the Inputbox extension, shown on [[Special:Version]].',
+	'inputbox-desc' => '{{desc}}',
 	'inputbox-error-bad-type' => '{{doc-important|"create", "comment", "search", "search2" and "fulltext" should not be translated.}}',
 	'tryexact' => 'Part of the "Inputbox" extension. This message is the text of the button to search the page you typed in the inputbox. If the page with the exact name exists, you will go directly to that page.',
 	'searchfulltext' => 'Part of the "Inputbox" extension. This message is the text of the button to search the page you typed in the inputbox. This button always goes to the search page, even if the page with the exact name exists.',
