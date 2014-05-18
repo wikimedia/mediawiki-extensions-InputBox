a1a1e64e462430e8faf98a1a014ba9499b59f6bd
Derk-Jan Hartman <hartman.wiki@gmail.com>
2014-05-18 11:07:27 +0200

    Inputbox: Some code conventions cleanup
    Change-Id: I16fcd16d81a7eecc845b5d36fd8af8e14792eea1

--- InputBox.hooks.php
+++ InputBox.hooks.php
@@ -22,7 +22,7 @@ class InputBoxHooks {
 		$request = $special->getRequest();
 		$prefix = $request->getText( 'prefix', '' );
 		$title = $request->getText( 'wpNewTitle', '' );
-		if( $special->getName() == 'Movepage' && $prefix !== '' && $title !== '' ) {
+		if ( $special->getName() == 'Movepage' && $prefix !== '' && $title !== '' ) {
 			$request->setVal( 'wpNewTitle', $prefix . $title );
 			$request->unsetVal( 'prefix' );
 		}
@@ -40,27 +40,27 @@ class InputBoxHooks {
 		// Return output
 		return $inputBox->render();
 	}
-	
+
 	/**
-	 * <inputbox type=create...> sends requests with action=edit, and 
-	 * possibly a &prefix=Foo.  So we pick that up here, munge prefix 
+	 * <inputbox type=create...> sends requests with action=edit, and
+	 * possibly a &prefix=Foo.  So we pick that up here, munge prefix
 	 * and title together, and redirect back out to the real page
 	 * @param $output OutputPage
 	 * @param $article Article
 	 * @param $title Title
 	 * @param $user User
-	 * @param $request WebRequest 
+	 * @param $request WebRequest
 	 * @param $wiki MediaWiki
 	 * @return bool
 	 */
-	public static function onMediaWikiPerformAction( 
-		$output, 
-		$article, 
-		$title, 
-		$user, 
-		$request, 
-		$wiki )
-	{
+	public static function onMediaWikiPerformAction(
+		$output,
+		$article,
+		$title,
+		$user,
+		$request,
+		$wiki
+	) {
 		if( $wiki->getAction( $request ) !== 'edit' ){
 			# not our problem
 			return true;
@@ -69,15 +69,15 @@ class InputBoxHooks {
 			# Fine
 			return true;
 		}
-		
+
 		$params = $request->getValues();
 		$title = $params['prefix'];
-		if ( isset( $params['title'] ) ){
+		if ( isset( $params['title'] ) ) {
 			$title .= $params['title'];
 		}
 		unset( $params['prefix'] );
 		$params['title'] = $title;
-		
+
 		global $wgScript;
 		$output->redirect( wfAppendQuery( $wgScript, $params ), '301' );
 		return false;
