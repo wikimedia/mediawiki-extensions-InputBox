c76ede7b4f3d4c20df7140644c3ed0af119928ed
Derk-Jan Hartman <hartman.wiki@gmail.com>
2014-05-18 11:53:13 +0200

    InputBox: Don't append id to class
    The search2 form type was adding the id to the classname. Probably a
    left over from a rewrite.
    
    Change-Id: I0f8c962a3fdadd7555c2b8add93e2b131aedece9

--- InputBox.classes.php
+++ InputBox.classes.php
@@ -283,7 +283,7 @@ class InputBox {
 				'type' => 'submit',
 				'name' => 'go',
 				'value' => $this->mButtonLabel,
-				'class' => 'bodySearchBtnGo' . $id
+				'class' => 'bodySearchBtnGo',
 			)
 		);
 
